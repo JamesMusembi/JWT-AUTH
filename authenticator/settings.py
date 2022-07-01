0069a6eb034531909683901eee8f43ae85dd5a1a
JamesMusembi <james.musembi@student.moringaschool.com>
2022-07-01 11:08:14 +0300

    editing urls

--- authenticator/settings.py
+++ authenticator/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'authenticator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postman',
+        'USER': 'moringa',
+        'PASSWORD': 'james',
     }
 }
 
